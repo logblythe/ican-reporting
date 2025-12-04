ea7d3a4dd5bf831c25768af9a4f1b41bdb255925
amrita <maharjanamrita94@gmail.com>
2025-12-04 14:36:02 +0545

    feat: adding default and not found page

---
+++ src/routes/PageNotFound.tsx
@@ -0,0 +1,19 @@
+import { Container, Stack, Text, Title, Button } from "@mantine/core";
+
+const PageNotFound = () => {
+  return (
+    <Container size="sm" style={{ textAlign: "center", marginTop: "10vh" }}>
+      <Stack align="center" spacing="md">
+        <Title order={1} size="4rem">
+          404
+        </Title>
+
+        <Text size="lg" c="dimmed">
+          Sorry, the page you are looking for does not exist.
+        </Text>
+      </Stack>
+    </Container>
+  );
+};
+
+export default PageNotFound;
